26258ae3898d459abd09504fe6541805482d722d
Andrea Piccioni <andpiccioni@gmail.com>
2017-10-07 21:43:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,8 +19,10 @@ function add2(n) {
   // Feel free to move things around!
 }
 
-function funkyFunction() {
-  return "FUNKY!"
+var funkyFunction = function() {
+  return function() {
+    return "FUNKY!"
+  }
 }
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
